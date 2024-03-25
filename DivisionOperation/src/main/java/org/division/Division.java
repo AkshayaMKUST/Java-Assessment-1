9b90cfe898ff5c6a838cd1d952dfc3a589dac989
AkshayaMKUST <127503224+AkshayaMKUST@users.noreply.github.com>
2024-03-25 18:13:59 +0530

    Update Division.java

--- DivisionOperation/src/main/java/org/division/Division.java
+++ DivisionOperation/src/main/java/org/division/Division.java
@@ -7,18 +7,18 @@ import java.util.Scanner;
 public class Division {
     public void performDivision()throws IncorrectDataPassedException {
             try{
-                Scanner sc = new Scanner(System.in);
-                System.out.println("Enter the divident : ");
-                int divident = sc.nextInt();
+                Scanner scanner = new Scanner(System.in);
+                System.out.println("Enter the dividend : ");
+                int dividend = scanner.nextInt();
                 System.out.println("Enter the divisor : ");
-                int divisor = sc.nextInt();
+                int divisor = scanner.nextInt();
                 if (divisor==0){
                     throw new IncorrectDataPassedException("Divisor you have entered is zero");
                 }
                 else{
-                    System.out.println("Divident used : "+ divident);
+                    System.out.println("Divident used : "+ dividend);
                     System.out.println("Divisor used : "+divisor);
-                    float result = (float) divident /divisor;
+                    float result = (float) dividend /divisor;
                     System.out.println("Division result : "+result);
                 }
             }
