ebe770e39a58379ad15a5d480f5f3a0df654228c
AkshayaMKUST <127503224+AkshayaMKUST@users.noreply.github.com>
2024-03-25 18:10:28 +0530

    Update Main.java

--- DivisionOperation/src/main/java/org/division/Main.java
+++ DivisionOperation/src/main/java/org/division/Main.java
@@ -3,13 +3,13 @@ package org.division;
 import org.division.exception.IncorrectDataPassedException;
 
 public class Main {
-    public static void main(String[] args) throws IncorrectDataPassedException {
+    public static void main(String[] args){
         try {
             Division division = new Division();
             division.performDivision();
         }
         catch (IncorrectDataPassedException e){
-            e.printStackTrace(System.out);
+           System.out.println("Error : "+ e.getMessage());
         }
     }
 }
